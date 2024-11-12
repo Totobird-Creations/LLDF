6b7732133c87dfa6433e380281787e7d3329c2b6
Totobird <>
2024-11-11 19:02:23 -0500

    Fixed actiontag from_string_unchecked generating bloated code

--- lldf-bind/src/__private/actiontag.rs
+++ lldf-bind/src/__private/actiontag.rs
@@ -1,3 +1,10 @@
+/// This is the most cursed thing ever.
+/// 
+/// Constraints:
+/// - Appear as an `enum` in documentation,
+/// - Needs to be usable as a [`String`](crate::types::String).
+/// - Needs to be able to transmute from an `&str`.
+/// - Needs to be able to transmute from a [`String`](crate::types::String).
 pub macro actiontag {
     (
 
@@ -8,39 +15,49 @@ pub macro actiontag {
 
     ) => { ::paste::paste!{
 
+
         #[repr(transparent)]
         #[crate::core::macros::derive(crate::core::clone::Clone, crate::core::marker::Copy)]
         $vis enum $ident {
             #[allow(private_interfaces)]
-            $ident( [< __ $ident >] )
+            Variant( [< __ $ident >] )
         }
-        #[doc(hidden)]
-        #[repr(transparent)]
-        #[allow(non_snake_case)]
-        #[crate::core::macros::derive(crate::core::clone::Clone, crate::core::marker::Copy)]
-        struct [< __ $ident >] {
-            $ident : &'static str
-        }
+
         impl $ident { $(
             #[doc = crate::core::concat!("`", $varvalue, "`")]
             #[allow(non_upper_case_globals)]
             $( #[$($varattrs)*] )*
-            $vis const $varident : Self = Self::$ident( [< __ $ident >] { $ident : $varvalue } );
+            $vis const $varident : Self = Self::Variant( [< __ $ident >] { StaticStr : $varvalue } );
         )* }
+
         impl crate::core::string::ToString for $ident {
             fn to_string(&self) -> crate::types::String {
-                let Self::$ident(variant) = self;
-                use crate::core::convert::From;
-                crate::types::String::from(variant.$ident)
+                let Self::Variant(variant) = self;
+                unsafe{ crate::core::mem::transmute_unchecked(variant.Opaque) }
             }
         }
+
         impl crate::core::string::FromStringUnchecked for $ident {
             #[inline(always)]
             unsafe fn from_string_unchecked(from : crate::types::String) -> Self {
-                #[allow(invalid_reference_casting)]
-                Self::$ident([< __ $ident >] { $ident : &*(&from as *const crate::types::String as *const &str) }) // TODO: This pointer magic is bloating the generated code.
+                use crate::types::DFValue;
+                Self::Variant([< __ $ident >] { Opaque : from.to_opaque() })
             }
         }
+
+
+        #[doc(hidden)]
+        #[allow(non_snake_case)]
+        #[crate::core::macros::derive(crate::core::marker::Copy)]
+        union [< __ $ident >] {
+            StaticStr : &'static str,
+            Opaque    : crate::__private::bind::DFOpaqueValue
+        }
+
+        impl crate::core::clone::Clone for [< __ $ident >] {
+            fn clone(&self) -> Self { Self { Opaque : unsafe { self.Opaque.clone() } } }
+        }
+
 
     } }
 }
--- lldf-bind/src/bind/mod.rs
+++ lldf-bind/src/bind/mod.rs
@@ -2,7 +2,7 @@ use crate::prelude::*;
 
 
 /// A value returned by a DiamondFire operation.
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 pub struct DFOpaqueValue {
     _opaque_type : u8
 }
